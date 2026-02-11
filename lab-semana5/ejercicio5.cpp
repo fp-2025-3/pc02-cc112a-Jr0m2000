fe0ccc6977188ba1a9abd42530579a7f30a734eb
= <=>
2026-02-11 18:06:34 -0500

    ejercicio 5 por completar

---
+++ lab-semana5/ejercicio5.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+struct Proceso
+{
+    int ID;
+    string Nombre;
+    float MB;
+};
+struct ListaProcesos
+{
+    
+};
+
+
+
+int main()
+{
+    Proceso* cabeza=nullptr;
+    
+
+    return 0;
+}
